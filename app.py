0057b116ceedecbff01c3ba6b6dcda36ac3d452e
code-vagabond <phung.linh2010@gmail.com>
2017-05-31 19:52:21 +0200

    Initial data mapping

--- app.py
+++ app.py
@@ -93,9 +93,6 @@ admin = flask_admin.Admin(
     app,
     index_view=AdminIndexView(
         name='Home',
-        endpoint='',
-        #template='admin/myhome.html',
-        #template='my_master.html',
 	template='admin/index.html',
         url='/',
         ),
@@ -103,14 +100,6 @@ admin = flask_admin.Admin(
     template_mode='bootstrap3'
 )
 
-
-# Create admin
-#admin = flask_admin.Admin(
-#    app,
-#    'Example: Auth',
-#    base_template='my_master.html',
-#    template_mode='bootstrap3',
-#)
 
 # Add model views
 admin.add_view(MyModelView(Role, db.session))
@@ -128,62 +117,12 @@ def security_context_processor():
     )
 
 
-def build_sample_db():
-    """
-    Populate a small db with some example entries.
-    """
-
-    import string
-    import random
-
-    db.drop_all()
-    db.create_all()
-
-    with app.app_context():
-        user_role = Role(name='user')
-        super_user_role = Role(name='superuser')
-        db.session.add(user_role)
-        db.session.add(super_user_role)
-        db.session.commit()
-
-        test_user = user_datastore.create_user(
-            first_name='Admin',
-            email='admin',
-            password=encrypt_password('admin'),
-            roles=[user_role, super_user_role]
-        )
-
-        first_names = [
-            'Harry', 'Amelia', 'Oliver', 'Jack', 'Isabella', 'Charlie', 'Sophie', 'Mia',
-            'Jacob', 'Thomas', 'Emily', 'Lily', 'Ava', 'Isla', 'Alfie', 'Olivia', 'Jessica',
-            'Riley', 'William', 'James', 'Geoffrey', 'Lisa', 'Benjamin', 'Stacey', 'Lucy'
-        ]
-        last_names = [
-            'Brown', 'Smith', 'Patel', 'Jones', 'Williams', 'Johnson', 'Taylor', 'Thomas',
-            'Roberts', 'Khan', 'Lewis', 'Jackson', 'Clarke', 'James', 'Phillips', 'Wilson',
-            'Ali', 'Mason', 'Mitchell', 'Rose', 'Davis', 'Davies', 'Rodriguez', 'Cox', 'Alexander'
-        ]
-
-        for i in range(len(first_names)):
-            tmp_email = first_names[i].lower() + "." + last_names[i].lower() + "@example.com"
-            tmp_pass = ''.join(random.choice(string.ascii_lowercase + string.digits) for i in range(10))
-            user_datastore.create_user(
-                first_name=first_names[i],
-                last_name=last_names[i],
-                email=tmp_email,
-                password=encrypt_password(tmp_pass),
-                roles=[user_role, ]
-            )
-        db.session.commit()
-    return
 
 if __name__ == '__main__':
 
     # Build a sample db on the fly, if one does not exist yet.
     app_dir = os.path.realpath(os.path.dirname(__file__))
     database_path = os.path.join(app_dir, app.config['DATABASE_FILE'])
-    if not os.path.exists(database_path):
-        build_sample_db()
 
     # Start app
     app.run(debug=True)
