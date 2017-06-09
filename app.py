ddb09dae6b5c856c2298cf21815338adaf04d46b
code-vagabond <phung.linh2010@gmail.com>
2017-06-09 16:00:54 +0200

    Create a separated model module

--- app.py
+++ app.py
@@ -13,10 +13,14 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from wtforms import StringField, PasswordField, SubmitField, validators
 from flask_wtf import FlaskForm
 
+from model import *
+
+
 # Create Flask application
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
-db = SQLAlchemy(app)
+#db = SQLAlchemy(app)
+db.init_app(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 user = current_user
@@ -54,50 +58,6 @@ class LoginForm(FlaskForm):
 
         self.user = user
         return True
-
-
-
-
-# Define models
-roles_users = db.Table(
-    'roles_users',
-    db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
-    db.Column('role_id', db.Integer(), db.ForeignKey('role.id'))
-)
-
-
-class Role(db.Model, RoleMixin):
-    id = db.Column(db.Integer(), primary_key=True)
-    name = db.Column(db.String(80), unique=True)
-    description = db.Column(db.String(255))
-
-    def __str__(self):
-        return self.name
-
-
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(255))
-    last_name = db.Column(db.String(255))
-    username = db.Column(db.String(255))
-    email = db.Column(db.String(255), unique=True)
-    password = db.Column(db.String(255))
-    active = db.Column(db.Boolean())
-    confirmed_at = db.Column(db.DateTime())
-    roles = db.relationship('Role', secondary=roles_users,
-                            backref=db.backref('users', lazy='dynamic'))
-
-#    def set_password(self, password):
-#        self.pw_hash = generate_password_hash(password)
-
-    #TODO: Use check_password hash for enduser
-    def check_password(self, password):
-        #return check_password_hash(self.password, password)
-        return self.password == password
-
-    def __str__(self):
-        return self.email
-
 
 # Setup Flask-Security
 user_datastore = SQLAlchemyUserDatastore(db, User, Role)
---
+++ model.py
@@ -0,0 +1,44 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_security import UserMixin, RoleMixin
+
+db = SQLAlchemy()
+
+# Define models
+roles_users = db.Table(
+    'roles_users',
+    db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
+    db.Column('role_id', db.Integer(), db.ForeignKey('role.id'))
+)
+
+
+class Role(db.Model, RoleMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+    description = db.Column(db.String(255))
+
+    def __str__(self):
+        return self.name
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(255))
+    last_name = db.Column(db.String(255))
+    username = db.Column(db.String(255))
+    email = db.Column(db.String(255), unique=True)
+    password = db.Column(db.String(255))
+    active = db.Column(db.Boolean())
+    confirmed_at = db.Column(db.DateTime())
+    roles = db.relationship('Role', secondary=roles_users,
+                            backref=db.backref('users', lazy='dynamic'))
+
+    #    def set_password(self, password):
+    #        self.pw_hash = generate_password_hash(password)
+
+    # TODO: Use check_password hash for enduser
+    def check_password(self, password):
+        # return check_password_hash(self.password, password)
+        return self.password == password
+
+    def __str__(self):
+        return self.email
